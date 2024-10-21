eb3d610e197e31c1e10c9432c4cf24118ae6a694
PERRIN Tom <120279211+TomPERRIN@users.noreply.github.com>
2024-10-21 15:43:07 +0200

    Premières idées et vote du thème.mk

---
+++ First.mk
@@ -0,0 +1,9 @@
+Thèmes :
+  - Langue des signes (3)(1)
+  - Restauration (1)
+  - Forumle 1 (1)
+  - Culture générale (1)(3)
+  - Programmation (0)
+
+  Langue des signes : cartes type vitesse sur des signes fondamentaux (quotidiens, français)
+  Culture générale : Plateau (cartes). Thème : Ingénieurerie (public étudiant / ingénieur; séparation par thèmes (info, physique, maths, ...), niveau de diff croissant) 
