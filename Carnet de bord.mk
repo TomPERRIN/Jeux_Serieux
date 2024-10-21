3272b44ec1c93613873878129b7b6f6117bddede
PERRIN Tom <120279211+TomPERRIN@users.noreply.github.com>
2024-10-21 16:22:58 +0200

    Create Carnet de bord.mk

---
+++ Carnet de bord.mk
@@ -0,0 +1,5 @@
+21/10
+La signification du prénom wafa. (Fidélité)
+Choix par votes du thème parmis ceux proposés, résultant au choix final de "Langue des signes"
+Choix du jeu définition (type de jeux, jouabilité, public cible). Jeux de type vitesse avec des cartes, à partir de 6 ans, grand publique, pas d'animateurs, objectif d'apprentissage de signes fondamentaux.
+Difficulté : prononciation du prenom "wafa"
