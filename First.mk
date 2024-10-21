2cffaa91c24d1939df7843549422dd897a7aaaac
PERRIN Tom <120279211+TomPERRIN@users.noreply.github.com>
2024-10-21 15:57:32 +0200

    Update First.mk

--- First.mk
+++ First.mk
@@ -5,15 +5,16 @@ Thèmes :
   - Culture générale (1)(3)
   - Programmation (0)
 
-Par vote le thème choisit est 
-
   1) Langue des signes : cartes type vitesse sur des signes fondamentaux (quotidiens, français)
   2) Restauration : jeux par équipe (cuisine, bar, sale, responsable), RP
   3) Formule 1 : Jeux de communication par équipe (pilote, opérateur, pit team), finir la course avec le moins de problème possible, dans le meilleur temps, certaines infos sont dispo que pour certains joueurs
   4) Culture générale : Plateau (cartes). Thème : Ingénieurerie (public étudiant / ingénieur; séparation par thèmes (info, physique, maths, ...), niveau de diff croissant) 
   5) Programmation : Vrai Faux sur des bouts de codes tirés (ou deviner la sortie)
 
+Choix retenus
+  1) Peu de jeux existants, facile en mise en place, publique plus large
+  4) Pas mal de jeux de ce genre, plus compliqué en mise en place
 
-1) Peu de jeux existants, facile en mise en place, publique plus large
-2) Pas mal de jeux de ce genre, plus compliqué en mise en place
-
+Choix final 
+  1) Langue des signes
+  
