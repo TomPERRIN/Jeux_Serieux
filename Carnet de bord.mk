02576b0bf5b000bd1c4a197b8524faf03464964f
PERRIN Tom <120279211+TomPERRIN@users.noreply.github.com>
2024-11-18 14:19:59 +0100

    Update Carnet de bord.mk

--- Carnet de bord.mk
+++ Carnet de bord.mk
@@ -11,3 +11,11 @@ Difficulté : Mise en accord pour le thème.
 Prévisions pour la séance prochaine : Choix des mots / signes, design des cartes, nom du jeu, se pencher sur les règles pour avoir une durée de jeu viable (pas trop longue)
 
 Equipe : Bonne entente et communication, mise en accord efficace; groupe multi culturel
+
+
+18/11
+Parmis les prévisions pour cette séance, définir l'ordre.
+En premier temps : 3 sur le choix des mots / signes et 2 sur le nom du jeu.
+En second temps : Les règles tous ensemble
+Ensuite : design
+Pour finir : relecture critique de ce qui a été fait
