02cb38166c8f7743f7523ca41a3004acb3fa3c4a
PERRIN Tom <120279211+TomPERRIN@users.noreply.github.com>
2024-12-02 17:31:02 +0100

    Update Règles.mk

--- Règles.mk
+++ Règles.mk
@@ -1,4 +1,16 @@
-Jeu de carte sur la langue des signes.
+Signe et gagne est un jeu de carte sur la langue des signes.
+
+Presentation : Pour apprendre les signes à partir de 6 ans et apprendre ce que représente l'apprentissage de la langue des signes 
 
 Nombre de joueurs : 2-6 joueurs
 
+Elements de jeu : 60 cartes mot, 30 cartes signes, 1 totem
+
+Règles :
+L'objectif est d'avoir été le plus réactif au cours de la partie en ayant attraper le totem en premier le plus de fois sans se tromper
+Le totem est placé au milieu avec les cartes signes
+
++1 si le totem est attrappé quand le mot à été retourné par l'un des joueurs.
+-3 si le totem est attrapé alors que le mot n'a pas été retourné.
+Distribution du même nombre de carte mot pour tout le monde. 
+La partie finie quand le nombre de signe définit est dépassé.
